bb98aa9f7b9eadd895908506d7b4cebe1a0aeff1
Ivan <bomak54@gmail.com>
2020-09-08 11:10:35 +0300

    creating a project structure

--- src/component/app/app.js
+++ src/component/app/app.js
@@ -1,9 +1,19 @@
 import React from "react";
 import HeaderInfo from "../header-info";
+import Eside from "../eside/eside";
+import Main from "../main";
+import Footer from "../footer-info";
 
 const App = () => {
     return (
-        <HeaderInfo/>
+        <div>
+            <HeaderInfo/>
+            <Eside/>
+            <Main/>
+            <Footer/>
+        </div>
+
+
     )
 };
 export default App;
--- src/component/header-info/header-info.js
+++ src/component/header-info/header-info.js
@@ -1,9 +1,37 @@
 import React, {Component} from "react";
+import logo from '../images/logo_col.svg'
+import './header-info.css'
 
 export default class HeaderInfo extends Component {
     render() {
         return (
-            <h1>hello</h1>
+            <header className='header'>
+                <section className='header-img'>
+                    <img src={logo} alt='logo'/>
+                </section>
+
+                <section className='header-lang-social'>
+                    <section className='lang'>
+                        <p>Berlin City Hall | Events and Tickets</p>
+                        <div className='header-link'>
+                            <p>Login</p>
+                            <p>DE</p>
+                            <p>RU</p>
+                        </div>
+                    </section>
+                    <section className='social'>
+                            <i className="fab fa-youtube"/>
+                            <i className="fab fa-facebook-square"/>
+                            <i className="fab fa-twitter"/>
+                            <i className="fab fa-instagram"/>
+                            <i className="fa fa-odnoklassniki" aria-hidden="true"/>
+
+                    </section>
+
+                </section>
+
+            </header>
         )
+
     };
 };
